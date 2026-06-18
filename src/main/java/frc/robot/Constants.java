69a270442633f7b4553f6e2a396b8caa570def75
Adriel <stammler.adriel@gmail.com>
2026-06-18 15:24:29 -0700

    Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,4 +14,10 @@ package frc.robot;
  */
 public final class Constants {
 
+    public static final int LEFT_MOTOR_PORT = 0;
+    public static final int RIGHT_MOTOR_PORT = 1;
+    public static final int LEFT_ENCODER_A = 4;
+    public static final int LEFT_ENCODER_B = 5;
+    public static final int RIGHT_ENCODER_A = 6;
+    public static final int RIGHT_ENCODER_B = 7;
 }
